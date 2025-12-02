7f5fce739b500edcde62ddb6275af75fac459410
dwong <leobridget6@gmail.com>
2025-12-02 21:02:37 +0700

    App: fix DB_NAME

--- frontend/connect_db.py
+++ frontend/connect_db.py
@@ -6,7 +6,7 @@ import requests
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 #DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DN_NAME", "currency_converter")
+DB_NAME = os.getenv("DB_NAME", "currency_converter")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 
